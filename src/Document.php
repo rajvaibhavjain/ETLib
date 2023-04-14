7b2578d3c9e96a615ca14bb1791ba508d5ee5e51
raj vaibhav jain <rajvaibhavjain@gmail.com>
2023-04-14 14:36:17 +0530

    CompressImage Function Update
    CompressImage Function Update

--- src/Document.php
+++ src/Document.php
@@ -35,6 +35,21 @@
         }
 
 
+        public static function CompressImage($source, $destination, $quality) {
+            $info = getimagesize($source);
+            if ($info['mime'] == 'image/jpeg') 
+                $image = imagecreatefromjpeg($source);
+            elseif ($info['mime'] == 'image/gif') 
+                $image = imagecreatefromgif($source);
+            elseif ($info['mime'] == 'image/png') 
+                $image = imagecreatefrompng($source);
+            if(imagejpeg($image, $destination, $quality)){
+                return true;
+            }else{
+                return false;
+            };
+        }
+
     }
 
 
